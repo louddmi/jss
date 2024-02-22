e42826be8f5873e0203df85b3ff080e0dce3539d
louddmi <145798874+louddmi@users.noreply.github.com>
2024-02-22 11:08:04 -0300

    Add files via upload

--- ejercicios04.js
+++ ejercicios04.js
@@ -2,32 +2,29 @@ function cantidadDeVocales(phrase){
 // Cuenta la cantidad de vocales en la frase pasada por parametros utilizando un bucle for
 // Tu código:👇
 let count = 0;
-    for (let i = 0; i < phrase.length; i++) {
+    for (let i = 0; i < phrase.length; i++) 
        
-        if (phrase[i] === "a" || phrase[i] === "e" || phrase[i] === "i" || phrase[i] === "o" || phrase[i] === "u") {
+        if (phrase[i] === "a" || phrase[i] === "e" || phrase[i] === "i" || phrase[i] === "o" || phrase[i] === "u") 
            
             count++;
-        }
-    }
+    
    
     return count;
 }
 
-
-
 function primerDivisible(divisor, dividendo) {
 // Encuentra el primer número divisible por 'num' a partir de un número dado utilizando un bucle while
 // Tu código:👇
-let numero = dividendo;
-while (numero % divisor !== 0) {
-    numero++;
+//let numero = dividendo;//
+while (divisor <= dividendo)  {  
+if ( dividendo % divisor !==0 )
+return divisor
+divisor++ 
 }
-return numero;
 }
-
 
 function encuentraX(x){
-//Realiza un algoritmo que recorre numeros del 1 al 100, cuando pase por el numero X pasado por parametros
+}//Realiza un algoritmo que recorre numeros del 1 al 100, cuando pase por el numero X pasado por parametros
 // retornar "El numero X es: ..."
 // Tu Código:👇
 
@@ -35,7 +32,7 @@ for ( let i=1; i <=100 ; i++) {
 if(i===x)
 return `el numero x es ${x}`
 }
-  }
+  
 function sumandoTodo(num){
 // Realiza un algoritmo que el cual recorra del 0 al numero pasado por parametros, debes retornar la suma de todo los
 // numeros que se recorrio
@@ -43,38 +40,17 @@ function sumandoTodo(num){
 let suma= 0
 for (let i=0; i<=num;i++)
 suma +=i
-return suma;
 }
+return suma;
 
 function BinarioADecimal(num) {
 // La funcion recibe por parametro, un numero binario pero en tipo de dato String
 // debes tomar este numero binario y ayudandote de un bucle pasarlo a binario
 // tu código:👇
 let decimal = 0;
-for (let i = 0; i < num.length; i++) {
-    let bit = parseInt(num[num.length - 1 - i]);
-    decimal += bit * Math.pow(2, i);
-}
-return decimal;
+for ()
 }
 
-function Fibonacci(n){
-// Realiza una secuencia Fibonacci con el indice pasado por paramentro
-// Se puede representar mediante la siguiente fórmula: F(n) = F(n-1) + F(n-2)
-// Tu código:👇
-if (n === 0) {
-    return [0];
-} else if (n === 1) {
-    return [0, 1];
-} else {
-    let secuencia = [0, 1];
-    for (let i = 2; i <= n; i++) {
-        let siguiente = secuencia[i - 1] + secuencia[i - 2];
-        secuencia.push(siguiente);
-    }
-    return secuencia;
-}
-}
 
 function esPrimo(numero) {
 //Escribe un programa que verifique si un número dado es primo o no.
@@ -87,7 +63,10 @@ for (let i =2; i <=  Math.sqrt(numero); i++)
 if (numero % 2 === 0)
 return `${numero}no es primo `
 return `${numero} es primo `
+
 }
+
+
 
 function invertirCadena(cadena) {
 //La función toma una cadena de texto como parámetro, devuelve la cadena invertida.
